0a34cf15bf4b699b831b14dd2d1f1ca490d22947
unknown <anggikhusnurida00@gmail.com>
2020-04-23 19:08:45 +0700

    Operator Aritmatika

---
+++ Operator Aritmatika.php
@@ -0,0 +1,15 @@
+<?php
+ echo "<H1>Operator Aritmatika</H1>";
+	echo "Nilai Angka = 8";
+	echo "<br>";
+	echo "Berapa Nilai dan jenis operator yang diperlukan jika nilai angka = 10 ?.";
+	echo "<br>";
+	echo "Berapa Nilai dan jenis operator yang diperlukan jika nilai angka = 6 ?.";
+	echo "<br>";
+	echo "Berapa Nilai dan jenis operator yang diperlukan jika nilai angka = 30 ?.";
+	echo "<br>";	
+	echo "Berapa Nilai dan jenis operator yang diperlukan jika nilai angka = 10 ?.";
+	echo "<br>";
+	echo "Jenis Operator yang diperlukan jika Nilai Angka = 11 ?.";
+	echo "Jenis Operator yang diperlukan jika Nilai Angka = 10 ?.";
+?>
